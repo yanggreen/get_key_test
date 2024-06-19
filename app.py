a93ef436e52aae7aca703f9a99925aa0dc28adfb
TeaYang <30551460+yanggreen@users.noreply.github.com>
2024-06-19 13:25:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from key_finder import get_key
 app = Flask(__name__)
-
+user_key = get_key()
 @app.route('/')
 def hello_world():
-    user_key = get_key()
+    print(user_key)
     return 'Hello, World!'
 
 
